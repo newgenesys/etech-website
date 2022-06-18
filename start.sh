2f38a88d0361231d2cf12c8da3f7104d32574459
Olouge <olongel@yahoo.com>
2022-06-18 12:20:09 +0100

    updated /glassfish5/glassfish/domains/domain1/autodeploy

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@
 /glassfish5/glassfish/bin/asadmin -u admin deploy /etech-website-0.0.1-SNAPSHOT.war
 #/usr/local/glassfish3/bin/asadmin stop-domain
 /glassfish5/glassfish/bin/asadmin stop-domain
-cp -R /etech-website-0.0.1-SNAPSHOT.war glassfish5/domains/domain1/autodeploy/
+cp -R /etech-website-0.0.1-SNAPSHOT.war /glassfish5/glassfish/domains/domain1/autodeploy
 # cp -R tmp/etech-website-0.0.1-SNAPSHOT.war /
 # /usr/local/glassfish4/bin/asadmin start-domain --verbose
 /glassfish5/glassfish/bin/asadmin start-domain --verbose
