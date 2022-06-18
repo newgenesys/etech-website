a28aa1e2618860128cea785f0599a2cf35c9b77a
Olouge <olongel@yahoo.com>
2022-06-18 12:08:16 +0100

    updated to glassfish5.0.1 and added offline copies of 4.1.2 and 3.1.2.2

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,13 @@
 # VERSION 0.1
 # DOCKER-VERSION 1.0.0
 
-FROM ubuntu:22.04
+# FROM ubuntu:22.04
 FROM java:8-jdk-alpine
 #FROM dockerfile/java
 
+ARG PORT=8989
+ARG ADMIN_PORT1=4848
+ARG ADMIN_PORT2=8181
 
 #RUN apt-get update -qq
 #RUN apt-get install -qq maven git
@@ -14,20 +17,30 @@ FROM java:8-jdk-alpine
 # COPY etech-website /
 
 WORKDIR /
+
+COPY start.sh .
+
+CMD cmod 777 /start.sh
+
 COPY target/etech-website-0.0.1-SNAPSHOT.war .
 #Downloading and unpacking GlassFish 312
-COPY glassfish-3.1.2.2.zip .
-#RUN wget http://download.java.net/glassfish/3.1.2.2/release/glassfish-3.1.2.2.zip
-RUN unzip glassfish-3.1.2.2.zip
-RUN rm -f glassfish-3.1.2.2.zip
-ENV PATH /glassfish3/glassfish/bin:$PATH
+#COPY glassfish-3.1.2.2.zip .
+# COPY glassfish-5.0.1.zip .
+RUN wget http://download.java.net/glassfish/5.0.1/release/glassfish-5.0.1.zip
+RUN unzip glassfish-5.0.1.zip
+RUN rm -f glassfish-5.0.1.zip
+ENV PATH /glassfish5/glassfish/bin:$PATH
 # RUN git clone https://github.com/spring-guides/gs-convert-jar-to-war-maven
 #Package the war file
 # WORKDIR /etech-website
 
 # RUN ./mvnw clean package
 
-RUN cp /etech-website-0.0.1-SNAPSHOT.war /glassfish3/glassfish/domains/domain1/autodeploy
+RUN cp /etech-website-0.0.1-SNAPSHOT.war /glassfish5/glassfish/domains/domain1/autodeploy
 
-EXPOSE 8080 4848 8181
-WORKDIR /glassfish3/glassfish/domains/domain1/logs
+
+EXPOSE ${PORT} ${ADMIN_PORT1} ${ADMIN_PORT2}
+
+ENTRYPOINT ["/start.sh"]
+
+WORKDIR /glassfish5/glassfish/domains/domain1/logs
--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-/usr/local/glassfish4/bin/asadmin start-domain
+# /usr/local/glassfish4/bin/asadmin start-domain
+/glassfish5/glassfish/bin/asadmin start-domain
 
-/usr/local/glassfish4/bin/asadmin create-jdbc-connection-pool --datasourceclassname=com.mysql.jdbc.jdbc2.optional.MysqlDataSource --restype=javax.sql.DataSource --property user=root:serverName=localhost:portNumber=3306:databaseName=etech_website:useSSL=false mysql_etech_website_rootPool
+# /usr/local/glassfish4/bin/asadmin create-jdbc-connection-pool --datasourceclassname=com.mysql.jdbc.jdbc2.optional.MysqlDataSource --restype=javax.sql.DataSource --property user=root:serverName=localhost:portNumber=3306:databaseName=etech_website:useSSL=false mysql_etech_website_rootPool
 
-/usr/local/glassfish4/bin/asadmin create-jdbc-resource --connectionpoolid mysql_etech_website_rootPool app/etech_mysql
-/usr/local/glassfish4/bin/asadmin -u admin deploy /etech-website-0.0.1-SNAPSHOT.war
-/usr/local/glassfish4/bin/asadmin stop-domain
-cp -R tmp/etech-website-0.0.1-SNAPSHOT.war glassfish/domains/domain1/autodeploy/etech-website-0.0.1-SNAPSHOT.war
-cp -R tmp/etech-website-0.0.1-SNAPSHOT.war /
-/usr/local/glassfish4/bin/asadmin start-domain --verbose
+# /usr/local/glassfish4/bin/asadmin create-jdbc-resource --connectionpoolid mysql_etech_website_rootPool app/etech_mysql
+# /usr/local/glassfish3/bin/asadmin -u admin deploy /etech-website-0.0.1-SNAPSHOT.war
+/glassfish5/glassfish/bin/asadmin -u admin deploy /etech-website-0.0.1-SNAPSHOT.war
+#/usr/local/glassfish3/bin/asadmin stop-domain
+/glassfish5/glassfish/bin/asadmin stop-domain
+cp -R /etech-website-0.0.1-SNAPSHOT.war glassfish5/domains/domain1/autodeploy/
+# cp -R tmp/etech-website-0.0.1-SNAPSHOT.war /
+# /usr/local/glassfish4/bin/asadmin start-domain --verbose
+/glassfish5/glassfish/bin/asadmin start-domain --verbose
